5acb7ae8439f2a3cd3719804b75657857f25095d
Sreerama Deepak Vutla <deepakvutla9@gmail.com>
2026-05-11 20:49:00 -0500

    Fix Express 5 wildcard route syntax for CORS

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-app.options('*', cors());
+app.options('/(.*)', cors());
 app.use(express.json());
 
 // Serve generated slide images
