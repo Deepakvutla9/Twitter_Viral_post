6ad7bf9e80c72b9f00a6d1d1d8287055de92445a
Sreerama Deepak Vutla <deepakvutla9@gmail.com>
2026-05-11 20:50:32 -0500

    Remove app.options wildcard causing Express 5 crash

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,6 @@ app.use(cors({
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-app.options('/(.*)', cors());
 app.use(express.json());
 
 // Serve generated slide images
